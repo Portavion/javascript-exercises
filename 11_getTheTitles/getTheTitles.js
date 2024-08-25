91d46f2933b7d7be8a3bfd464eee48fe83f31333
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-25 18:30:36 +0200

    Solve exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titleArray = [];
+    array.forEach ( book => titleArray.push(book.title));
+    return titleArray;
 };
 
 // Do not edit below this line
