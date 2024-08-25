8077750c552de7f37d4f8b2d1c6ff98adcfd843d
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-25 18:55:30 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(originalArray) {
+    let ageArray = [];
+    let date = new Date();
+    originalArray.forEach(person => {
+        if (person.yearOfDeath !== undefined){
+            ageArray.push({name:person.name, age:(person.yearOfDeath - person.yearOfBirth)});
+        }
+        else{
+            ageArray.push({name:person.name, age:(date.getFullYear() - person.yearOfBirth)});
+        }
+    })
+    ageArray.sort((a,b) => (a.age-b.age)>=0 ? -1 : 1);
+    console.log(ageArray);
+    return ageArray[0];
 };
 
 // Do not edit below this line
