cf950556eaccb858195225d61e797c5a66dd641b
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-25 18:21:31 +0200

    solve exerise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(element) {
+    intElement = parseInt(element);
+    let first = 0;
+    let second = 0;
+    let third = 1;
 
+    if (intElement === 0) {
+        return 0;
+    }
+    else if (intElement < 0){
+        return 'OOPS';
+    }
+    for (let i = 2; i<=intElement;i++){
+        first = second;
+        second = third;
+        third = first + second;
+    }
+    return third;
 };
 
 // Do not edit below this line
