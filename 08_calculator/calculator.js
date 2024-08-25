eb06524b897c586a6245ac87e60f92107e8fbdb7
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-25 17:48:02 +0200

    solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...number) {
+	return number.reduce((total,a) => (total+a),0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  if (number.length === 0){
+    return 0;
+  }
+  else {
+    return number.reduce((total,a) => (total+a),0,);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  if (number.length === 0){
+    return 0;
+  }
+  else {
+    return number.reduce((total,a) => (total*a),1,);
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  if (a === 0){
+    return 1;
+  }
+  else {
+    for (let i = a; i > 0; i--){
+      total = total*i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
