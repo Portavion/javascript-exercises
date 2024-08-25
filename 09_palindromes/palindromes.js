018567d09d45007e22af1400af7efd6081c1a158
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-25 18:11:19 +0200

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let nopunctWord = (word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')).toLowerCase();
+    nospaceWord = nopunctWord.replace(/\s/g, '');
+    let array = nospaceWord.split('');
 
+    let reversedArray = array.toReversed();
+    console.log(array);
+    console.log(reversedArray);
+
+    if (reversedArray.toString() == array.toString()){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
